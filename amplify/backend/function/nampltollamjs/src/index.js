6a75719dbd304c6132370a064be3567e4efd9c20
asnegur <bbs.md@mail.ru>
2023-12-05 00:37:18 +0200

    Nodejs3

--- amplify/backend/function/nampltollamjs/src/index.js
+++ amplify/backend/function/nampltollamjs/src/index.js
@@ -4,12 +4,14 @@
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 exports.handler = async (event) => {
+    console.log('<<<___S T A R T____L A M B D A___>>>')
     console.log(`EVENT: ${JSON.stringify(event)}`);
     let arg = ''
     if (event['arguments']) {
         console.log(`arguments: ${JSON.stringify(event['arguments'])}`);
         arg = JSON.stringify(event['arguments']);
     }
+    console.log('<<<___F I N I S H____L A M B D A___>>>')
     return {
         statusCode: 200,
     //  Uncomment below to enable CORS requests
