f3819d188011347edfc45017a4179723622cddfd
asnegur <bbs.md@mail.ru>
2023-12-04 23:27:10 +0200

    Added node js lambda

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,36 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "nextampltol": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "nextampltol3fb11ac6": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "nampltollambda": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "nampltollamjs": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
---
+++ src/API.ts
@@ -0,0 +1,25 @@
+/* tslint:disable */
+/* eslint-disable */
+//  This file was automatically generated and should not be edited.
+
+export type CvData = {
+  bucketName: string,
+  objectKey: string,
+  source: string,
+};
+
+export type MyCustomMutationMutationVariables = {
+  namet?: string | null,
+};
+
+export type MyCustomMutationMutation = {
+  myCustomMutation?: string | null,
+};
+
+export type MyCustomQueryQueryVariables = {
+  cvData?: CvData | null,
+};
+
+export type MyCustomQueryQuery = {
+  myCustomQuery?: string | null,
+};
